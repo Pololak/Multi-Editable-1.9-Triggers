56191d6e4d151c4fa2d5ce7afd56b61e8e04635b
Pololak <pololakhldm@gmail.com>
2025-01-27 23:41:38 +0300

    g

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/EditorUI.hpp>
+
+class $modify(PolzEditorUI, EditorUI) {
+	void showPopup(ColorSelectPopup* parent) {
+		auto* director = CCDirector::sharedDirector();
+		auto* currentScene = director->getRunningScene();
+
+		log::info("{}", director);
+	}
+
+	void editObject(CCObject* p0) {
+		for (auto obj : CCArrayExt<EffectGameObject*>(m_selectedObjects)) {
+			switch (obj->m_objectID)
+			{
+				case 29: // 0x1d
+				case 30: // 0x1e
+				case 104: // 0x68 
+				case 105: // 0x69
+				case 744: // 0x2e8
+				case 900: // 0x384
+				case 915: // Line trigger ??? // 0x393
+					auto colorSelectPopup = ColorSelectPopup::create(obj, m_selectedObjects, nullptr);
+					colorSelectPopup->m_colorPicker->setColorValue(obj->m_triggerTargetColor);
+					CCDirector::sharedDirector()->getRunningScene()->addChild(colorSelectPopup, 105);
+					//showPopup(colorSelectPopup);
+					return;
+			}
+		}
+		EditorUI::editObject(p0);
+	}
+};
